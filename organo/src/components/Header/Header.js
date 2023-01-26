f557bb0269be48a3d1e4acb3d6055727bb391ff4
Anna Luiza <luizafistarol@gmail.com>
2023-01-26 13:34:10 -0300

    feat(Header): href logo

--- organo/src/components/Header/Header.js
+++ organo/src/components/Header/Header.js
@@ -3,14 +3,18 @@ import './Header.css';
 export const Header = () => {
     return (
         <header className='header'>
-            <h1><img src="/images/logo.svg" alt="Logo principal da página ComicStore" /></h1>
+            <h1>
+                <a href="https://github.com/annaluizacamargo/ComicStore">
+                    <img src="/images/logo.svg" alt="Logo principal da página ComicStore" />
+                </a>
+            </h1>
             <div className='menu'>
                 <div className='user'>
                     <i class="fa-solid fa-user"></i>
                     <h4>Olá, Anna</h4>
                 </div>
                 <div className='wish-list'>
-                    <h4>Wish List</h4>                    
+                    <h4>Wish List</h4>
                 </div>
                 <button className='btn-cart'>
                     <i class="fa-solid fa-cart-shopping"></i>
