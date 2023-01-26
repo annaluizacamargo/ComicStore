acd43239c87469bd7426fc55c5268d7f7e7aac5c
Anna Luiza <luizafistarol@gmail.com>
2023-01-26 12:36:38 -0300

    feat(Header): add menu

--- organo/src/components/Header/Header.js
+++ organo/src/components/Header/Header.js
@@ -4,6 +4,17 @@ export const Header = () => {
     return (
         <header className='header'>
             <h1><img src="/images/logo.svg" alt="Logo principal da página ComicStore" /></h1>
+            <div className='menu'>
+                <div className='user'>
+                    <i class="fa-solid fa-user"></i>
+                    <h4>Olá, Anna</h4>
+                </div>
+                <h4>Wish List</h4>
+                <div className='cart'>
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <h4>Cart</h4>
+                </div>
+            </div>
         </header>
     )
 }
