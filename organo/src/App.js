2022344ab779a4aba20edec6c8aedf6f73322f68
Anna Luiza <luizafistarol@gmail.com>
2023-01-27 12:11:46 -0300

    feat(Comic): style

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Comic } from './components/Comic/Comic';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Comic />
+      <Comic />
+      <Comic />
+      <Comic />
+      <Comic />
+      <Comic />
+      <Comic />
+      <Comic />
     </div>
   );
 }
--- organo/src/components/Comic/Comic.js
+++ organo/src/components/Comic/Comic.js
@@ -3,8 +3,8 @@ import './Comic.css';
 export const Comic = () => {
     return (
         <section className='comic'>
-            <img href='https://comicboom.com.br/loja/wp-content/uploads/2018/11/guardioes-da-galaxia-1.jpg'></img>
-            <div className='descrição'>
+            <img src='https://comicboom.com.br/loja/wp-content/uploads/2018/11/guardioes-da-galaxia-1.jpg'/>
+            <div className='descricao'>
                 <h4>Título (ano)</h4>
                 <p>Issue</p>
                 <h3>$3.99</h3>
