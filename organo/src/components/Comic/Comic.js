f3a6f1df348a3e4d061d26cba7b003908127d3ac
Anna Luiza <luizafistarol@gmail.com>
2023-01-27 11:37:53 -0300

    feat(Comic): add return

--- organo/src/components/Comic/Comic.js
+++ organo/src/components/Comic/Comic.js
@@ -2,6 +2,17 @@ import './Comic.css';
 
 export const Comic = () => {
     return (
-        console.log("comic")
+        <section className='comic'>
+            <img href='https://comicboom.com.br/loja/wp-content/uploads/2018/11/guardioes-da-galaxia-1.jpg'></img>
+            <div className='descrição'>
+                <h4>Título (ano)</h4>
+                <p>Issue</p>
+                <h3>$3.99</h3>
+            </div>
+            <div className='btn-action'>
+                <button>Buy</button>
+                <i class="fa-regular fa-bookmark"></i>
+            </div>
+        </section>
     )
 }
