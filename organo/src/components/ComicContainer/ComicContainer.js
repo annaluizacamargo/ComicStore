e01feb4b0023d484d83908a76b29a765cf691b3d
Anna Luiza <luizafistarol@gmail.com>
2023-01-29 00:59:08 -0300

    feat(ComicContainer): remove carousel

--- organo/src/components/ComicContainer/ComicContainer.js
+++ organo/src/components/ComicContainer/ComicContainer.js
@@ -2,9 +2,6 @@ import { Comic } from '../Comic/Comic';
 import './ComicContainer.css';
 import md5 from 'md5';
 import Api from '../../services/index';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import {responsive} from './ConfigCarousel'
 
 const getComics = async () => { //!PASSAR PARÂMETRO DA VARIÁVEL
     const ts = Date.now();
@@ -14,13 +11,13 @@ const getComics = async () => { //!PASSAR PARÂMETRO DA VARIÁVEL
     const params = {
         ts,
         apikey,
-        hash: md5(ts + privateKey + apikey),        
+        hash: md5(ts + privateKey + apikey),
         titleStartsWith: 'avengers', //!SUBSTITUIR POR VARIÁVEL
         orderBy: '-focDate',
     }
 
     try {
-        const response = await Api.get('/v1/public/comics', {params})
+        const response = await Api.get('/v1/public/comics', { params })
         console.log(response)
     } catch (error) {
         console.error(error)
@@ -31,8 +28,8 @@ export const ComicContainer = () => {
     //getComics();
     return (
         <div className='comicContainer'>
-            <h3>Guardians Of The Galaxy</h3>        
-            <Carousel responsive={responsive}>
+            <h3>Guardians Of The Galaxy</h3>
+            <div>
                 <Comic></Comic>
                 <Comic></Comic>
                 <Comic></Comic>
@@ -40,7 +37,7 @@ export const ComicContainer = () => {
                 <Comic></Comic>
                 <Comic></Comic>
                 <Comic></Comic>
-            </Carousel>
+            </div>
         </div>
     )
 }
