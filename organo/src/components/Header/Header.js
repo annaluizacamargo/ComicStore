72f54601d6f4f1cecd7bd68faeee4988e7f75afd
Anna Luiza <luizafistarol@gmail.com>
2023-01-29 20:35:22 -0300

    feat: add Router

--- organo/src/components/Header/Header.js
+++ organo/src/components/Header/Header.js
@@ -1,18 +1,23 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
     return (
         <header className='header'>
-            <h1>
-                <img src="/images/logo.svg" alt="Logo principal da página ComicStore" />
-            </h1>
+
+            <Link to='/' className='logo'>
+                <h1>
+                    <img src="/images/logo.svg" alt="Logo principal da página ComicStore" />
+                </h1>
+            </Link>
+
             <div className='menu'>
                 <div className='user'>
                     <i class="fa-solid fa-user"></i>
                     <h4>Olá, Anna</h4>
                 </div>
                 <div className='wish-list'>
-                    <h4>Wish List</h4>
+                    <Link to='/wish-list' className='h4-wish-list'>Wish List</Link>
                 </div>
                 <button className='btn-cart'>
                     <i class="fa-solid fa-cart-shopping"></i>
--- organo/src/index.js
+++ organo/src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Header from './components/Header';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "wish-list",
+    element: <Header />,
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
