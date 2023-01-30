4e3ee046a94a94f55c9b36495e5166f7d39f32b6
Anna Luiza <luizafistarol@gmail.com>
2023-01-30 20:37:13 -0300

    fix: error (some) localstorage

--- organo/src/components/Comic/Comic.js
+++ organo/src/components/Comic/Comic.js
@@ -6,34 +6,37 @@ import { DeleteLocalStorage, SaveLocalStorage } from './LocalStorage';
 
 
 export const Comic = (props) => {
-    //console.log(props.comic)
     const buy = () => {
-        console.log("oi")
+        //_ TO DO
+        console.log("to do")
     }
 
     useEffect(() => {
         const arrayComicsLocalStorage = JSON.parse(localStorage.getItem('isSaved'));
-        const comicIsSaved = arrayComicsLocalStorage.some(value => value.title === props.comic.title);
-        setIsSave(comicIsSaved);
+
+        if (arrayComicsLocalStorage != null) {
+            const comicIsSaved = arrayComicsLocalStorage.some(value => value.title === props.comic.title);
+            setIsSave(comicIsSaved);
+        }
     }, [])
 
     const save = (comic) => {
         setIsSave(!isSave);
-        !isSave? SaveLocalStorage(comic) : DeleteLocalStorage(comic);
+        !isSave ? SaveLocalStorage(comic) : DeleteLocalStorage(comic);
     };
 
     const [isSave, setIsSave] = useState(false)
 
     return (
         <div className='comic' key={props.comic.title}>
-            <img src={props.comic.image + '.' +props.comic.imageExtension}/>
+            <img src={props.comic.image + '.' + props.comic.imageExtension} />
             <div className='descricao'>
                 <h4>{props.comic.title}</h4>
                 <h3>${props.comic.price}</h3>
             </div>
             <div className='btn-action'>
                 <button onClick={buy}>Buy</button>
-                <i className={isSave? "fa-solid fa-bookmark" : "fa-regular fa-bookmark"} onClick={() => save(props.comic)}></i>
+                <i className={isSave ? "fa-solid fa-bookmark" : "fa-regular fa-bookmark"} onClick={() => save(props.comic)}></i>
             </div>
         </div>
     )
