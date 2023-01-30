50ce65de9eeba52c89d7d6dc6eaa292b0edddcc2
Anna Luiza <luizafistarol@gmail.com>
2023-01-30 17:32:37 -0300

    feat: set isSave with one key in localstorage

--- organo/src/components/Comic/LocalStorage.js
+++ organo/src/components/Comic/LocalStorage.js
@@ -1,10 +1,22 @@
+//export const SaveLocalStorage = (comic) => {
+//    localStorage.setItem(comic.title, JSON.stringify(comic));
+//}
+
+
 export const SaveLocalStorage = (comic) => {
-    localStorage.setItem(comic.title, JSON.stringify(comic));
-    
+    let localStorageValue = JSON.parse(localStorage.getItem('isSaved'));
+
+    if (localStorageValue == null) {
+        localStorage.setItem('isSaved', JSON.stringify([comic]))
+    } else {
+        localStorageValue.push(comic)
+        localStorage.setItem('isSaved', JSON.stringify(localStorageValue))
+    }
 }
 
 export const DeleteLocalStorage = (comic) => {
-    localStorage.removeItem(comic.title);
+    let localStorageValue = JSON.parse(localStorage.getItem('isSaved'));
+    const indexRemove = localStorageValue.findIndex(obj => obj.title === comic.title)
+    localStorageValue.splice((indexRemove), 1)
+    localStorage.setItem('isSaved', JSON.stringify(localStorageValue))
 }
-
-
