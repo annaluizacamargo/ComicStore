ea4034c99dd71ffb6bedb3cc5c21fbe481449f59
Anna Luiza <luizafistarol@gmail.com>
2023-01-31 20:13:10 -0300

    feat(App): refactoring

--- organo/src/App.js
+++ organo/src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import ComicContainer from './components/ComicContainer';
 import Header from './components/Header';
 
-const nameSections = ['Guardians of the Galaxy', 'Shang-Chi', 'Doctor Strange', 'Black Panther']
+const nameSections = ['Guardians of the Galaxy', 'Shang-Chi', 'Doctor Strange', 'Black Panther'];
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {nameSections.map((nameSection) => (<ComicContainer nameSection={nameSection} key={nameSection}/>))}
+      {nameSections.map((nameSection) => (<ComicContainer nameSection={nameSection} key={nameSection} />))}
     </div>
   );
 }
