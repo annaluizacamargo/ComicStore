4b8d62751ce3ea69a6c7b83227946f4767284fc9
Anna Luiza <luizafistarol@gmail.com>
2023-03-05 17:31:39 -0300

    feat: remove assets from react-app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  Link,
 } from "react-router-dom";
 import WishList from './content/WishList';
 
@@ -28,5 +25,3 @@ root.render(
     <RouterProvider router={router} />
   </React.StrictMode>
 );
-
-reportWebVitals();
